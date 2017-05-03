65cd8ad26826e1af5fbb94aaf75893678b481663
Åke Engelbrektson <eson57@users.noreply.github.com>
2017-05-04 00:30:46 +0200

    Create harbour-bikeme-sv.ts (#1)
    * Create harbour-bikeme-sv.ts

---
+++ translations/harbour-bikeme-sv.ts
@@ -0,0 +1,246 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>Find a bike in your city!</source>
+        <translation>Hitta en cykel i din stad!</translation>
+    </message>
+    <message>
+        <source>Version %1</source>
+        <translation>Version %1</translation>
+    </message>
+    <message>
+        <source>This app helps you to find a bike or a free parking spot for various self-service bicycle schemes arround the world:&lt;ul&gt;&lt;li&gt;JCDecaux&lt;/li&gt;&lt;li&gt;Nextbike&lt;/li&gt;&lt;li&gt;Smoove&lt;/li&gt;&lt;li&gt;And more!&lt;/li&gt;&lt;/ul&gt;BikeMe provides a simple interactive map displaying all stations by city, their number of available bikes and free parking spots, and the geolocation.&lt;br /&gt;Click on %1 to activate the geolocation and on %2 to center the map on your position.&lt;br/&gt;Regular users: Save your favourites stations and see their status directly on the cover!</source>
+        <translation>Denna app hjälper dig att hitta en cykel eller en ledig parkeringsplats i olika självbetjäningssystem för cyklar runt om i världen: &lt;ul&gt;&lt;li&gt;JCDecaux&lt;/li&gt;&lt;li&gt;Nextbike&lt;/li&gt;&lt;li&gt;Smoove&lt;/li&gt;&lt;li&gt;och fler!&lt;/li&gt;&lt;/ul&gt;BikeMe tillhandahåller en enkel interaktiv karta som visar alla stationer sorterade efter stad, antal tillgängliga cyklar på stationen, lediga parkeringsplatser och geografisk plats.&lt;br /&gt;Tryck %1 för att aktivera geolokalisering och %2, för att centrera kartan på din position.&lt;br/&gt;Regelbundna användare: Spara dina favoritstationer och se deras status direkt på programminiatyren!</translation>
+    </message>
+</context>
+<context>
+    <name>CitiesBrowser</name>
+    <message>
+        <source>About BikeMe</source>
+        <translation>Om BikeMe</translation>
+    </message>
+    <message>
+        <source>Display: Cities browser</source>
+        <translation>Visa: Stadsläsare</translation>
+    </message>
+    <message>
+        <source>Refresh cities list</source>
+        <translation>Uppdatera stadslistan</translation>
+    </message>
+    <message>
+        <source>Welcome to BikeMe!</source>
+        <translation>Välkommen till BikeMe!</translation>
+    </message>
+    <message>
+        <source>Pull down to load cities list</source>
+        <translation>Dra neråt för att läsa in stadslistan.</translation>
+    </message>
+    <message>
+        <source>Cities</source>
+        <translation>Städer</translation>
+    </message>
+</context>
+<context>
+    <name>CitiesList</name>
+    <message>
+        <source>Fetching operators...</source>
+        <translation>Hämtar operatörer...</translation>
+    </message>
+    <message>
+        <source>Cities</source>
+        <translation>Städer</translation>
+    </message>
+    <message>
+        <source>Filter</source>
+        <translation>Filtrera</translation>
+    </message>
+    <message>
+        <source>Welcome to BikeMe!</source>
+        <translation>Välkommen till BikeMe!</translation>
+    </message>
+    <message>
+        <source>Pull down to load cities list</source>
+        <translation>Dra neråt för att läsa in stadslistan.</translation>
+    </message>
+    <message>
+        <source>About BikeMe</source>
+        <translation>Om BikeMe</translation>
+    </message>
+    <message>
+        <source>Update cities list</source>
+        <translation>Uppdatera stadslistan</translation>
+    </message>
+</context>
+<context>
+    <name>Favourites</name>
+    <message>
+        <source>Updated: %1</source>
+        <translation>Uppdaterad: %1</translation>
+    </message>
+    <message>
+        <source>Closed</source>
+        <translation>Stängd</translation>
+    </message>
+    <message>
+        <source>Move Up</source>
+        <translation>Flytta upp</translation>
+    </message>
+    <message>
+        <source>Move Down</source>
+        <translation>Flytta ner</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Ta bort</translation>
+    </message>
+    <message>
+        <source>Deleting</source>
+        <translation>Tar bort</translation>
+    </message>
+    <message>
+        <source>Add a new favourite</source>
+        <translation>Lägg till en ny favorit</translation>
+    </message>
+    <message>
+        <source>Adding a favourite</source>
+        <translation>Lägger till en favorit</translation>
+    </message>
+    <message>
+        <source>Station %1 does not exist</source>
+        <translation>Station %1 finns inte</translation>
+    </message>
+    <message>
+        <source>Favourites - %1</source>
+        <translation>Favoriter - %1</translation>
+    </message>
+    <message>
+        <source>Update all</source>
+        <translation>Uppdatera alla</translation>
+    </message>
+    <message>
+        <source>Update</source>
+        <translation>Uppdatera</translation>
+    </message>
+    <message>
+        <source>Individual update not available for this city</source>
+        <translation>Individuell uppdatering ej tillgänglig för denna stad</translation>
+    </message>
+</context>
+<context>
+    <name>InteractiveMap</name>
+    <message>
+        <source>Refreshing...</source>
+        <translation>Uppdaterar...</translation>
+    </message>
+    <message>
+        <source>Updated: %1</source>
+        <translation>Uppdaterad: %1</translation>
+    </message>
+    <message>
+        <source>Updating...</source>
+        <translation>Uppdaterar...</translation>
+    </message>
+    <message>
+        <source>Updated:</source>
+        <translation>Uppdaterad:</translation>
+    </message>
+    <message>
+        <source>Loading stations...</source>
+        <translation>Läser in stationer...</translation>
+    </message>
+    <message>
+        <source>Can&apos;t display all status for this city.</source>
+        <translation>Kan inte visa all status för denna stad.</translation>
+    </message>
+    <message>
+        <source>Only &quot;all status&quot; mode available for this city.</source>
+        <translation>Endast alla stationers status är tillgängligt för denna stad.</translation>
+    </message>
+</context>
+<context>
+    <name>ProgressInfoBar</name>
+    <message numerus="yes">
+        <source>%n Cities</source>
+        <translation>
+            <numerusform>%n stad</numerusform>
+            <numerusform>%n städer</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <source>%n Error(s)</source>
+        <translation>
+            <numerusform>%n fel</numerusform>
+            <numerusform>%n fel</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>SecondPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Stations Map</source>
+        <translation>Stationskarta</translation>
+    </message>
+    <message>
+        <source>Favourites</source>
+        <translation>Favoriter</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Map provider</source>
+        <translation>Kartleverantör</translation>
+    </message>
+    <message>
+        <source>Start page</source>
+        <translation>Startsida</translation>
+    </message>
+    <message>
+        <source>Clear cache</source>
+        <translation>Rensa cache</translation>
+    </message>
+    <message>
+        <source>BikeMe keeps in cache stations location to save mobile data. If stations don&apos;t display, you can try to clear the cache to remove potentially corrupted files.</source>
+        <translation>BikeMe cache-lagrar stationspositioner för att spara mobildata. Om stationer inte visas, kan du försöka rensa cache, för att ta bort pontentiellt skadade filer.</translation>
+    </message>
+    <message>
+        <source>Display all stations status</source>
+        <translation>Visa alla stationers status</translation>
+    </message>
+    <message>
+        <source>All station status will be downloaded and displayed on the map. Consume more data.</source>
+        <translation>Alla stationers status kommer att laddas ner och visas på kartan. Förbrukar mer data.</translation>
+    </message>
+    <message>
+        <source>Default</source>
+        <translation>Standard</translation>
+    </message>
+    <message>
+        <source>Map</source>
+        <translation>Karta</translation>
+    </message>
+    <message>
+        <source>Favourites</source>
+        <translation>Favoriter</translation>
+    </message>
+    <message>
+        <source>Clearing cache</source>
+        <translation>Rensar cache</translation>
+    </message>
+</context>
+</TS>
